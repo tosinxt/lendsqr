6ea24f36c6805fd8f23019bf0c22723bcc2019ac
SafeKids Admin <allioluwatosin8@gmail.com>
2025-07-26 13:08:38 +0100

    Initial project setup with TypeScript, ESLint, and Prettier

---
+++ .eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint', 'prettier'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+  ],
+  env: {
+    node: true,
+    es2021: true,
+  },
+  rules: {
+    'prettier/prettier': 'error',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
+    'no-console': 'warn',
+  },
+};
